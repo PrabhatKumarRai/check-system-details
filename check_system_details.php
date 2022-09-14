c6b359d09f3ac11c3bc26e6a37e95bd135a939e5
Prabhat Kumar Rai <pkr.prabhat1998@gmail.com>
2022-09-14 12:14:10 +0530

    inital commit

---
+++ check_system_details.php
@@ -0,0 +1,81 @@
+<?php
+/* 
+ * Plugin Name:       Check System Details
+ * Plugin URI:        https://github.com/PrabhatKumarRai/check-system-details
+ * Description:       Easily check your WordPress installation and server details along with database tables, installed plugins, and the active theme.
+ * Version:           1.0.0
+ * Author:            Prabhat Rai
+ * Author URI:        https://github.com/PrabhatKumarRai/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Requires at least: 4.9
+ * Requires PHP:      5.6
+*/
+
+// Adds the plugin to the WordPress admin menu/sidebar
+add_action('admin_menu', 'check_system_details_menu');
+function check_system_details_menu(){
+    add_menu_page(
+        'Check System Details',
+        'System Details',
+        'administrator',
+        'check_system_details',
+        'check_system_details_data',
+        'dashicons-admin-settings'
+    );
+}
+
+function check_system_details_data(){
+    include_once plugin_dir_path(__FILE__) . 'admin/check_system_details_data.php';
+}
+
+// CSS for the plugin page
+function check_system_details_css(){
+    echo "<style type='text/css'>
+    .check-system-details-container{
+    padding: 25px 25px 0 25px;
+    margin-left: -25px;
+    margin-top: -16px;
+    }
+    .check-system-details-head{
+        position: relative;
+    }
+    .check-system-details-head h1{
+        position: absolute;
+        width: 100%;
+    }
+    .check-system-details-body{
+        margin-top: 55px;
+    }
+    .check-system-details-title,
+    .check-system-details-footer{
+        text-decoration: underline;
+        text-align: center;
+    }
+    .check-system-details-container h2{
+        padding: 10px;
+        margin-bottom: 0;
+        border: 1px solid black;
+        border-bottom: 0;
+    }
+    .check-system-details-section{
+        background-color: #fff;
+    }
+    .check-system-details-container table{
+        border: 1px solid black;
+        width: 100%;
+        margin-bottom: 30px;
+        padding: 5px;
+        word-break: break-all;
+    }
+    .check-system-details-container table td{
+        padding: 5px;
+        font-size: 16px;
+        width: 50%;
+    }
+    .check-system-details-plugin-details table td{
+        width: unset;
+    }
+    </style>";
+}
+add_action( 'admin_head', 'check_system_details_css' );
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Check System Details ===
+Contributors: prabhatrai
+Tags: server details,wordpress details,database details
+Requires at least: 4.7
+Tested up to: 6.0.2
+Stable tag: 1.0.0
+Requires PHP: 7.0
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily check your WordPress installation and server details along with database tables, installed plugins, and the active theme.
+
+== Description =
+### Check System Details - Easily Check Your Installation and Server Details ###
+
+##No Setup Required##
+
+## The details include:  ##
+
+#WordPress Details#
+* WordPress address
+* Site address
+* WordPress version
+* WordPress multisite
+* WordPress memory
+* WordPress debug
+* WordPress debug
+* WordPress cron
+* WordPress Language
+
+#Server Details#
+* Max input timeout
+* Max input vars
+* Max file upload
+* Max file upload
+
+#Database Details#
+* Database Name
+* Database Host
+* Database Charset
+* Database Prefix
+
+#Names of existing Database Tables#
+
+#Current Theme Details#
+* Name
+* Version
+* Description
+* Author
+
+#Installed Plugin Details#
+* Plugin name with hyperlink to plugin's website
+* Version
+* Author
+
+##A quick overview of the plugin:##
+[youtube https://youtu.be/VTXt1PNqXkU]
+
+== Screenshots ==
+1. Plugin's first screen with WordPress and Server details.
+2. Database details along with the list of database tables.
+3. Details on the active theme and installed plugins.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial version
